916045fa183999c3a66ecc1b8ecca71cf3b99089
ruaimmohammed <20me0115@vvce.ac.in>
2021-03-31 11:24:39 +0000

    Program 8

--- Program-8.c
+++ Program-8.c
@@ -20,6 +20,8 @@ void main()
   pt = arr1; // pt store the base address of array arr1
   for (i = 0; i < n; i++) {
       // write the code to calculate sum
+      sum=sum+*pt;
+      pt++;
      
    }
   printf("Sum=%d",sum);
